c157dcc8ce3c98383690f6c5048d328ee6538e6f
akihiko47 <akihiko4701a@gmail.com>
2023-12-05 04:03:55 +0300

    Initial graph

---
+++ Graph/Assets/Scripts/Graph.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+	[SerializeField]
+	Transform pointPrefab;
+
+	[SerializeField, Range(10, 100)]
+	int resolution = 10;
+
+	private void Awake()
+	{
+		float step = 2f / resolution;
+		var scale = Vector3.one * step;
+		Vector3 position = Vector3.zero;
+
+		int i = 0;
+		for (i = 0; i < resolution; i++)
+		{
+			Transform point = Instantiate(pointPrefab);
+			position.x = (i + 0.5f) * step - 1f;
+			position.y = position.x * position.x;
+			point.localPosition = position;
+			point.localScale = scale;
+			point.SetParent(transform, false);
+		}
+	}
+}
