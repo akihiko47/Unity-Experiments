b9ec7a6c649a48a406719b83412d8a0df0a9d808
akihiko47 <akihiko4701a@gmail.com>
2023-12-07 21:20:38 +0300

    New surfaces

--- Graph/Assets/Scripts/Graph Library.cs
+++ Graph/Assets/Scripts/Graph Library.cs
@@ -4,31 +4,77 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary
 {
 
-    public delegate float Function(float x, float t);
+    public delegate Vector3 Function(float u, float v, float t);
 
-    public enum FunctionName { Wave, MultiWave, Ripple }
-    static Function[] functions = {Wave, MultiWave, Ripple};
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus, Cone }
+    static Function[] functions = {Wave, MultiWave, Ripple, Sphere, Torus, Cone};
 
     public static Function GetFunction (FunctionName name)
     {
         return functions[(int)name];
     }
-    public static float Wave(float x, float t)
+
+    public static Vector3 Wave(float u, float v, float t)
     {
-        return Sin(PI * (x + t));
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
     }
 
-    public static float MultiWave(float x, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Sin(PI * (x +  0.5f * t));
-        y += Sin(PI * 2f * (x + t)) * 0.5f;
-        return y * (2f / 3f);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u +  0.5f * t));
+        p.y += Sin(PI * 2f * (v + t)) * 0.5f;
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= (1f / 2.5f);
+        p.z = v;
+        return p;
     }
 
-    public static float Ripple(float x, float t)
+    public static Vector3 Ripple(float u, float v, float t)
     {
-        float d = Abs(x);
-        float y = Sin(PI * (4f * d - t));
-        return y / (1f + 10f * d);
+        Vector3 p;
+        p.x = u;
+        float d = Sqrt(u*u + v*v);
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= (1f + 10f * d);
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 Sphere(float u, float v, float t)
+    {
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+        float s = r * Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    public static Vector3 Torus(float u, float v, float t)
+    {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    public static Vector3 Cone(float u, float v, float t)
+    {
+        Vector3 p;
+        p.x = 1 * (Cos(v) + u * Cos(v / 2) * Cos(v));
+        p.y = 1 * (Sin(v) + u * Cos(v / 2) * Sin(v));
+        p.z = 1 * u * Sin(v / 2);
+        return p;
     }
 }
--- Graph/Assets/Scripts/Graph.cs
+++ Graph/Assets/Scripts/Graph.cs
@@ -16,15 +16,11 @@ public class Graph : MonoBehaviour {
 	{
 		float step = 2f / resolution;
 		var scale = Vector3.one * step;
-		Vector3 position = Vector3.zero;
 
-		int i = 0;
-		points = new Transform[resolution];
-		for (i = 0; i < resolution; i++)
+		points = new Transform[resolution * resolution];
+		for (int i = 0; i < points.Length; i++)
 		{
 			Transform point = points[i] = Instantiate(pointPrefab);
-			position.x = (i + 0.5f) * step - 1f;
-			point.localPosition = position;
 			point.localScale = scale;
 			point.SetParent(transform);
 		}
@@ -33,16 +29,20 @@ public class Graph : MonoBehaviour {
     private void Update()
     {
 		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-
+		float step = 2f / resolution;
 		float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
-        {
-			Transform point = points[i];
-			Vector3 position = point.localPosition;
+		float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+		{
+			if (x == resolution)
+			{
+				x = 0;
+				z += 1;
+				v = (z + 0.5f) * step - 1f;
+			}
 
-			position.y = f(position.x, time);
-
-			point.localPosition = position;
+			float u = (x + 0.5f) * step - 1f;
+			points[i].localPosition = f(u, v, time);
 		}
-    }
+	}
 }
