dd78f0858c1e003e87b883866979e73578d2e79c
akihiko47 <akihiko4701a@gmail.com>
2023-12-29 18:11:29 +0300

    Manual fractal positions to compute buffer

--- Fractal/Assets/Scripts/Fractal Script.cs
+++ Fractal/Assets/Scripts/Fractal Script.cs
@@ -20,68 +20,91 @@ public class Fractal : MonoBehaviour {
     };
 
     struct FractalPart {
-        public Vector3 direction;
-        public Quaternion rotation;
-        public Transform transform;
+        public Vector3 direction, worldPosition;
+        public Quaternion rotation, worldRotation;
+        public float spinAngle;
     }
 
     FractalPart[][] parts;
 
+    Matrix4x4[][] matrices;
 
-    private void Awake() {
+    ComputeBuffer[] matricesBuffers;
+
+
+    private void OnEnable() {
         parts = new FractalPart[depth][];
+        matrices = new Matrix4x4[depth][];
+        matricesBuffers = new ComputeBuffer[depth];
 
         int length = 1;
         for (int i = 0; i < depth; i++) {
             parts[i] = new FractalPart[length];
+            matrices[i] = new Matrix4x4[length];
+            matricesBuffers[i] = new ComputeBuffer(length, 16 * 4);
             length *= 5;
         }
 
-        float scale = 1f;
-        parts[0][0] = CreatePart(0, 0, scale);
+        parts[0][0] = CreatePart(0);
         for (int li = 1; li < parts.Length; li++) {  // level iterator
-            scale *= 0.5f;
             FractalPart[] levelParts = parts[li];
             for (int fpi = 0; fpi < levelParts.Length; fpi++) {  // fractal part indicator
-                levelParts[fpi] = CreatePart(li, fpi % 5, scale);
+                levelParts[fpi] = CreatePart(fpi % 5);
             }
+        }
+    }
+
+    private void OnDisable() {
+        for (int i = 0; i < depth; i++) {
+            matricesBuffers[i].Release();
+        }
+        parts = null;
+        matrices = null;
+        matricesBuffers = null;
+    }
+
+    private void OnValidate() {
+        if (parts != null && enabled) {
+            OnDisable();
+            OnEnable();
         }
     }
 
     private void Update() {
-        Quaternion deltaRotation = Quaternion.Euler(0f, 22.5f * Time.deltaTime, 0f);
+        float spinAngleDelta = 22.5f * Time.deltaTime;
         FractalPart rootPart = parts[0][0];
-        rootPart.rotation = rootPart.rotation * deltaRotation;
-        rootPart.transform.localRotation = rootPart.rotation;
+        rootPart.spinAngle += spinAngleDelta;
+        rootPart.worldRotation = rootPart.rotation * Quaternion.Euler(0f, rootPart.spinAngle, 0f);
         parts[0][0] = rootPart;
+        matrices[0][0] = Matrix4x4.TRS(rootPart.worldPosition, rootPart.worldRotation, Vector3.one);
 
+        float scale = 1f;
         for (int li = 1; li < parts.Length; li++) {
+            scale *= 0.5f;
             for (int fpi = 0; fpi < parts[li].Length; fpi++) {
-                Transform parentTransform = parts[li - 1][fpi / 5].transform;
+                FractalPart parent = parts[li - 1][fpi / 5];
                 FractalPart part = parts[li][fpi];
 
-                part.rotation *= deltaRotation;
-                part.transform.localRotation = parentTransform.rotation * part.rotation;
+                part.spinAngle += spinAngleDelta;
+                part.worldRotation = parent.worldRotation * (part.rotation * Quaternion.Euler(0f, part.spinAngle, 0f));
 
-                part.transform.localPosition = parentTransform.localPosition + parentTransform.localRotation * (1.5f * part.transform.localScale.x * part.direction);
+                part.worldPosition = parent.worldPosition + parent.worldRotation * (1.5f * scale * part.direction);
 
                 parts[li][fpi] = part;
+                matrices[li][fpi] = Matrix4x4.TRS(part.worldPosition, part.worldRotation, scale * Vector3.one);
             }
+        }
+
+        for (int i = 0; i < depth; i++) {
+            matricesBuffers[i].SetData(matrices[i]); 
         }
     }
 
-    FractalPart CreatePart(int levelIndex, int childIndex, float scale) {
-        GameObject go = new GameObject("Fractal Part L" + levelIndex + " C" + childIndex);
-        go.transform.SetParent(transform, false);
-        go.AddComponent<MeshFilter>().mesh = mesh;
-        go.AddComponent<MeshRenderer>().material = material;
-        go.transform.localScale = scale * Vector3.one;
-
+    FractalPart CreatePart(int childIndex) {
         return new FractalPart
         {
             direction = directions[childIndex],
             rotation = rotations[childIndex],
-            transform = go.transform
         };
     }
 }
