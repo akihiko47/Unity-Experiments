94388dc05af0fe4864e471b4f3ddf09df66d226b
akihiko47 <akihiko4701a@gmail.com>
2024-01-01 02:07:41 +0300

    Timed spawning with coroutine

--- Meshes/Assets/Scripts/Grid.cs
+++ Meshes/Assets/Scripts/Grid.cs
@@ -6,4 +6,32 @@ public class Grid : MonoBehaviour
 {
     [SerializeField]
     int xSize, ySize;
+
+    private Vector3[] vertices;
+
+    private void Awake() {
+        StartCoroutine(Generate());
+    }
+    private void OnDrawGizmos() {
+        if (vertices == null) {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere(transform.TransformPoint(vertices[i]), 0.1f);
+        }
+    }
+
+    private IEnumerator Generate() {
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+
+        for (int y = 0, i = 0; y < ySize; y++, i++) {
+            for (int x = 0; x < xSize; x++, i++) {
+                vertices[i] = new Vector3(x, y, 0f);
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+    }
 }
