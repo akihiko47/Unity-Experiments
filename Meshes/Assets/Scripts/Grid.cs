8af72587a58b5fe3e9d9d34d9e5c9375fd5e6688
akihiko47 <akihiko4701a@gmail.com>
2024-01-01 17:54:26 +0300

    Animated procedural mesh

--- Meshes/Assets/Scripts/Grid.cs
+++ Meshes/Assets/Scripts/Grid.cs
@@ -9,9 +9,29 @@ public class Grid : MonoBehaviour
 
     private Vector3[] vertices;
 
+    private Mesh mesh;
+
+	private float timeElapsed = 0f;
+
     private void Awake() {
-        StartCoroutine(Generate());
+        Generate();
     }
+
+    private void Update() {
+
+		Animate();
+
+		timeElapsed += Time.deltaTime;
+    }
+
+	private void Animate() {
+		for (int i = 0; i < vertices.Length; i++) {
+			vertices[i].z = Mathf.Sin(timeElapsed * Mathf.PI + vertices[i].x + vertices[i].y) - 1f;
+		}
+
+		GetComponent<MeshFilter>().mesh.vertices = vertices;
+	}
+
     private void OnDrawGizmos() {
         if (vertices == null) {
             return;
@@ -24,14 +44,27 @@ public class Grid : MonoBehaviour
         }
     }
 
-    private IEnumerator Generate() {
-        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+	private void Generate() {
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		mesh.name = "Procedural Grid";
 
-        for (int y = 0, i = 0; y < ySize; y++, i++) {
-            for (int x = 0; x < xSize; x++, i++) {
-                vertices[i] = new Vector3(x, y, 0f);
-                yield return new WaitForSeconds(0.05f);
-            }
-        }
-    }
+		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		for (int i = 0, y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++, i++) {
+				vertices[i] = new Vector3(x, y);
+			}
+		}
+		mesh.vertices = vertices;
+
+		int[] triangles = new int[xSize * ySize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+			}
+		}
+		mesh.triangles = triangles;
+	}
 }
